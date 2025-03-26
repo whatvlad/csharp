672a7499b834a767daa1c5138b84f9253d1a1910
Vladyslav Tkachenko <vladennopwnz@gmail.com>
2025-03-26 12:22:43 +0000

    Add files via upload

---
+++ CSProjects/SimpleConsoleHealthManaBar/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int health = 10, maxHealth = 10;
+            int mana = 7, maxMana = 7;
+
+            while (true)
+            {
+                Draw(health, maxHealth, ConsoleColor.Red, 0, '|');
+                Draw(mana, maxMana, ConsoleColor.Blue, 1);
+
+                Console.SetCursorPosition(0, 5);
+                Console.Write("Введите число(хп): ");
+                health += Convert.ToInt32(Console.ReadLine());
+                Console.Write("Введите число(мана): ");
+                mana += Convert.ToInt32(Console.ReadLine());
+                Console.ReadKey();
+                Console.Clear();
+            }
+
+        }
+
+        static void Draw(int value, int maxValue, ConsoleColor color, int position, char symbol = '_')
+        {
+            ConsoleColor defaultColor = Console.BackgroundColor;
+
+            string bar = "";
+
+            for (int i = 0; i < value; i++)
+            {
+                bar += symbol;
+            }
+            Console.SetCursorPosition(0, position);
+            Console.Write('[');
+            Console.BackgroundColor = color;
+            Console.Write(bar);
+            Console.BackgroundColor = defaultColor;
+
+            bar = "";
+
+            for (int i = value; i < maxValue; i++)
+            {
+                bar += symbol;
+            }
+
+            Console.Write(bar + ']');
+        }
+    }
+}
